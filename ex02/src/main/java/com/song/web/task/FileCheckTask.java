ad6eb40bff4e04e8c2ff7860044e96e341459e3b
hojunking <gkwlal3@naver.com>
2021-10-20 19:36:56 +0900

    song 1020

--- ex02/src/main/java/com/song/web/task/FileCheckTask.java
+++ ex02/src/main/java/com/song/web/task/FileCheckTask.java
@@ -1,6 +1,5 @@
 package com.song.web.task;
 
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import lombok.extern.java.Log;
@@ -8,10 +7,10 @@ import lombok.extern.java.Log;
 @Component
 @Log
 public class FileCheckTask {
-	
-	@Scheduled(cron = "0/10 * * * *") /* (fixedDelay = 5000) /->기간, ,는 1분기준  */
-	public void checkFile() {
-		log.info("file----check!");
-		log.info("==================");
-	}
+
+	/*
+	 * @Scheduled(cron = "0/10 * * * *") (fixedDelay = 5000) /->기간, ,는 1분기준 public
+	 * void checkFile() { log.info("file----check!");
+	 * log.info("=================="); }
+	 */
 }
