03e1e3a00a40c24970942e6114a1338596c306bd
hojun <84840627+hojunking@users.noreply.github.com>
2022-03-08 15:35:18 +0900

    update1

---
+++ BootProject/src/main/java/com/song/bootproject/BootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.song.bootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootProjectApplication.class, args);
+    }
+
+}
