4c13c573800c4addb72abf5772e17074052f72d3
hojun <84840627+hojunking@users.noreply.github.com>
2022-03-10 20:51:40 +0900

    0310 update2

--- BootProject/src/test/java/com/song/bootproject/BootProjectApplicationTests.java
+++ BootProject/src/test/java/com/song/bootproject/BootProjectApplicationTests.java
@@ -3,8 +3,8 @@ package com.song.bootproject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.web.WebAppConfiguration;
-
 @SpringBootTest
+
 class BootProjectApplicationTests {
 
     @Test
